68c4d41fe7384ad9be9c4ec6410d350e1e06d33a
Amir-namjoy <amir.namjoy@gmail.com>
2023-03-09 21:18:54 +0330

    IDataBaseContext

--- Amir_Store.Application/Interfaces/Contexts/IDataBaseContext.cs
+++ Amir_Store.Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using Amir_Store.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Amir_Store.Application.Interfaces.Contexts
 {
-    internal interface IDataBaseContext
+    public interface IDataBaseContext
     {
          DbSet<User> Users { get; set; }
          DbSet<Role> Roles { get; set; }
          DbSet<UserInRole> UserInRoles { get; set; }
+
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken));
+
     }
 }
