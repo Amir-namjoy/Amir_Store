b0b06430a33d629738156b5b5861a38789b62436
Amir-namjoy <amir.namjoy@gmail.com>
2023-04-13 22:41:19 +0330

    Add Category

---
+++ Amir_Store.Application/Services/Users/Queries/GetUsers/GetUsersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Amir_Store.Application.Services.Users.Queries.GetUsers
+{
+    public class GetUsersDTO
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+
+}
---
+++ Amir_Store.Application/Services/Users/Queries/GetUsers/GetUsersService.cs
@@ -0,0 +1,34 @@
+﻿using Amir_Store.Application.Interfaces.Contexts;
+using Amir_Store.Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Amir_Store.Application.Services.Users.Queries.GetUsers
+{
+    public class GetUsersService : IGetUsersService
+    {
+        private readonly IDataBaseContext _contex;
+        public GetUsersService(IDataBaseContext contex)
+        {
+            _contex = contex;
+        }
+
+        public List<GetUsersDTO> Execute(RequestGetUsetDto request)
+        {
+            var users = _contex.Users.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(request.SearchKey))
+            {
+                users = users.Where(p => p.FullName.Contains(request.SearchKey) && p.Email.Contains(request.SearchKey));
+            }
+            int rowsCount = 0;
+            return users.ToPaged(request.Page, 20, out rowsCount).Select(p => new GetUsersDTO
+            {
+                Email = p.Email,
+                FullName = p.FullName,
+                Id = p.Id,
+
+            }).ToList();
+        }
+    }
+
+}
